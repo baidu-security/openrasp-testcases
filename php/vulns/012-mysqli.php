2a994f4c5a69c3b647a2600504d95fb03f11a45a
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-03-12 13:41:40 +0800

    Let us release that way

--- php/vulns/012-mysqli.php
+++ php/vulns/012-mysqli.php
@@ -46,7 +46,7 @@
 				<p>第一步: 请以mysql root账号执行下面的语句创建表</p>
 				<pre>DROP DATABASE IF EXISTS test;
 CREATE DATABASE test;					
-grant all privileges on test.* to 'test'@'%'' identified by 'test';
+grant all privileges on test.* to 'test'@'%' identified by 'test';
 CREATE TABLE test.vuln (id INT, name text);
 INSERT INTO test.vuln values (0, "openrasp");
 INSERT INTO test.vuln values (1, "rocks");
