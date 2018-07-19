c447ca0aa6c7af1e70a772efdddeeea0dc20abce
Aaron Lewis <the.warl0ck.1989@gmail.com>
2018-07-19 14:21:25 +0800

    修正010格式

--- php/vulns/010-file-include.php
+++ php/vulns/010-file-include.php
@@ -15,16 +15,16 @@
 <body>
 	<h1>010 - 任意文件包含 - include 拼接方式</h1>
 
-	<p>正常调用: </p>
+	<p>正常调用:</p>
 	<pre>curl '<a href="<?php echo $normal; ?>" target="_blank"><?php echo $normal; ?></a>'</pre>
 	<br>
 	
-	<p>不正常调用 (</p>
+	<p>不正常调用:</p>
 	<pre>curl '<a href="<?php echo $linux1; ?>" target="_blank"><?php echo $linux1; ?></a>'</pre>
 
 	<br>
 	<p>包含内容</p>
-<?php 
+<?php
 	if (isset ($_GET['file']))
 	{
 		include (__DIR__ . '/classes/' . $_GET['file']);
